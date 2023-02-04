67174a36a9a34013aa453f0c45052e029d7d34cc
phipenguu <paul_ong@dlsu.edu.ph>
2023-02-04 21:38:47 +0800

    First Area Almost Done

---
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerBehavior : MonoBehaviour
+{
+    public static GameManagerBehavior Instance { get; private set; }
+
+
+    private void Awake()
+    {
+        if (Instance == null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
