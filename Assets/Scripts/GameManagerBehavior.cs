92d0b9e1b88bafdf75968ce7f39ef0224e5ebe13
phipenguu <paul_ong@dlsu.edu.ph>
2023-02-05 10:19:35 +0800

    Added Health

--- Assets/Scripts/GameManagerBehavior.cs
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -1,34 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Micosmo.SensorToolkit;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerBehavior : MonoBehaviour
 {
     public static GameManagerBehavior Instance { get; private set; }
 
+    [SerializeField] private TriggerSensor trigger;
 
+    [SerializeField] private GameObject player;
+    [SerializeField] private int maxPlayerHealth = 3;
     private void Awake()
     {
         if (Instance == null && Instance != this)
         {
-            Destroy(this);
-        }
-        else
-        {
             Instance = this;
-            DontDestroyOnLoad(this.gameObject);
         }
+
+        DontDestroyOnLoad(this.gameObject);
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
+        trigger.OnDetected.AddListener(SwitchToFirstBoss);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SwitchToFirstBoss(GameObject gameObject, Sensor sensor)
+    {
+        SceneManager.LoadScene("First Boss Area");
+        player.GetComponent<Health>().playerHealth = maxPlayerHealth;
+    }
+
+    public void SwitchToSecondBoss()
+    {
+        SceneManager.LoadScene("Second Boss Area");
+        player.GetComponent<Health>().playerHealth = maxPlayerHealth;
     }
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Image = UnityEngine.UI.Image;
+
+public class Health : MonoBehaviour
+{
+    public int playerHealth;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < playerHealth)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
